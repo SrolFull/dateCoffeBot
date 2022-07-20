921e9fd436eda1b65746e497cec767c47f75078a
SrolFull <srolfull@gmail.com>
2022-07-20 22:47:01 +0500

    Подготовил проект к работе

---
+++ src/main/java/starter/AppStarter.java
@@ -0,0 +1,15 @@
+package starter;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppStarter {
+
+  private static final Logger logger = LoggerFactory.getLogger(AppStarter.class);
+
+
+  public static void main(String[] args) {
+    //todo Старт и настройка приложения
+  }
+}
