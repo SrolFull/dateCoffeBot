2c9cb843d6cccd6c4f89d1f3bbf50bdd36011227
SrolFull <srolfull@gmail.com>
2022-07-21 13:38:42 +0500

    Настроил логирование

--- src/main/java/starter/AppStarter.java
+++ src/main/java/starter/AppStarter.java
@@ -5,11 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AppStarter {
-
   private static final Logger logger = LoggerFactory.getLogger(AppStarter.class);
 
-
   public static void main(String[] args) {
-    //todo Старт и настройка приложения
+    logger.debug("String app");
   }
 }
