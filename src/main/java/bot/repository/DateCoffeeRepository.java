86f204574c3740e8ca3ff996a9b66aabdb2566a3
SrolFull <srolfull@gmail.com>
2022-08-09 01:53:34 +0500

    Починил базу

--- src/main/java/bot/repository/DateCoffeeRepository.java
+++ src/main/java/bot/repository/DateCoffeeRepository.java
@@ -5,11 +5,10 @@ import bot.models.db.Users;
 import java.util.HashMap;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 public interface DateCoffeeRepository extends JpaRepository<Users, Long> {
   Users deleteUserById(Long id);
 
-  @Query(value = "select chatId from Users")
+  @Query(value = "select chatId, currentCommand from Users")
   HashMap<Long, String> getAllChatIdAndCurrentCommand();
 }
